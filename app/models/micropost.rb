b628eaa77e80e77bb528327b0d6cbe29108fb349
Yudhishthir <yudhishthir@poplify.com>
2022-09-02 20:11:42 +0530

    [ActionText Implementation]:  -Installed active_storage for storing images.  -Added rich text content for micropost by implementing action   text.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+  has_rich_text :rich_content
 
   validates :content, presence: true
   validates :user_id, presence: true
